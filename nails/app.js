720d315ca32bd7d387c90bd1bb03d2b3684adfff
Aline Gruba <alinegruba@Alines-MacBook-Air.local>
2022-03-28 01:09:45 -0300

    nails

--- nails/app.js
+++ nails/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(5000, () => console.log('Servidor corriendo en el puerto 5000'))
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
@@ -31,6 +31,6 @@ app.get('/carrito', (req, res) => {
     res.sendFile(path.join(__dirname, './views/carrito.html'));
 })
 
-app.get('/car', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/carrito2.html'));
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productpage.html'));
 })
