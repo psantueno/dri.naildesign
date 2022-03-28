6ca1d16fd5206f6c208a1b4bf49c242fa4b2b6f5
dlalba282 <dlalba282@gmail.com>
2022-03-28 01:13:31 -0500

    Se realiza ajuste de header, logo y sintetizaciòn de media Queries para header

--- nails/app.js
+++ nails/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(5000, () => console.log('Servidor corriendo en el puerto 5000'))
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 5000'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
