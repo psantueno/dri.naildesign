f26a6ef5d00dacffb7f2d6da554aa580222e8589
dlalba282 <dlalba282@gmail.com>
2022-04-18 07:22:00 -0500

    feature: registro y login con rutas y controller a renderizar archivos ejs desde la carpeta views en la raiz, en calse preguntar sobre src

--- nails/app.js
+++ nails/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const rutasUsuarios = require("./src/routers/users.js")
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'))
 
+//Se configura el view engine EJS
+app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, './src/views/users'))
+
+// Actualizaciòn de las rutas Acceso a login y register
+app.use('/', rutasUsuarios);
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
-})
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
 })
 
 app.get('/menu', (req, res) => {
@@ -23,19 +27,12 @@ app.get('/banner', (req, res) => {
     res.sendFile(path.join(__dirname, './views/bannerhome.html'));
 })
 
-app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/registro.html'));
-})
-
 app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/carrito.html'));
+    res.sendFile(path.join(__dirname, './src/views/products/carrito.html'));
 })
 
 app.get('/product', (req, res) => {
     res.sendFile(path.join(__dirname, './views/productpage.html'));
-})
-app.get('/product1', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productpageNew.html'));
 })
 
 app.get('/servicios', (req, res) => {
