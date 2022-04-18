0f8103655125c8f9ed2ca9d157f97b4b91c11342
Aline Gruba <alinegruba@Alines-MacBook-Air.local>
2022-04-18 00:38:45 -0300

    se add form de products back en html y css

--- nails/app.js
+++ nails/app.js
@@ -8,34 +8,35 @@ app.use(express.static(publicPath));
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.sendFile(path.join(__dirname, './src/views/home.html'));
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
-})
-
-app.get('/menu', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/menu.html'));
+    res.sendFile(path.join(__dirname, './src/views/users/login.html'));
 })
 
 app.get('/banner', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/bannerhome.html'));
+    res.sendFile(path.join(__dirname, './src/views/partials/bannerhome.html'));
 })
 
 app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/registro.html'));
+    res.sendFile(path.join(__dirname, './src/views/users/registro.html'));
 })
 
 app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/carrito.html'));
+    res.sendFile(path.join(__dirname, './src/views/products/carrito.html'));
 })
 
 app.get('/product', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productpage.html'));
+    res.sendFile(path.join(__dirname, './src/views/products/productpage.html'));
 })
+
 app.get('/product1', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productpageNew.html'));
+    res.sendFile(path.join(__dirname, './src/views/products/agregarproduct.html'));
+})
+
+app.get('/product2', (req, res) => {
+    res.sendFile(path.join(__dirname, './src/views/products/editarproduct.html'));
 })
 
 app.get('/servicios', (req, res) => {
