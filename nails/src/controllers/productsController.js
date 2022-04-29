658cf8b43b2a2cc56bbd7f151bf824c84ad967a3
psantueno <seba.antueno@gmail.com>
2022-04-29 12:01:39 -0300

    Se crea base data de maquillaje y se aplica dinamismo en dicha section

--- nails/src/controllers/productsController.js
+++ nails/src/controllers/productsController.js
@@ -1,13 +1,16 @@
+//array con lista de esmaltes
 const listaDeProductos = require("../database/BaseDatosProducto");
-
+//array con lista de maquillaje
+const listaDeProductos2 = require("../database/BaseDatosProducto2");
+//array con titles de las distintas views para aplicar en el head.
 const titlesOfPages = ["Shop", "Editar Productos", "Agregar productos", "Detalle del Producto"];
-
+// array con las hojas de estilos para aplicar en el head.
 const stylesOfPages = ['rel=stylesheet href=/css/detailProduct.css', 'rel=stylesheet href=/css/formsproducts.css'];
 
 const productsController = { 
     
     listProducts: (req, res) => {
-        res.render("listProductsDinamic", {title: titlesOfPages[0], style1:"", style2:"", listaProductosEnviar:listaDeProductos});
+        res.render("listProducts", {title: titlesOfPages[0], style1:"", style2:"", listaEsmaltes: listaDeProductos, listaMaquillaje: listaDeProductos2});
     },
 
     detailProduct: (req, res) => {
---
+++ nails/src/database/BaseDatosProducto2.js
@@ -0,0 +1,45 @@
+const listaDeProductos2 = [
+    {
+        nombre: "Paleta de Maquillaje",
+        precio: "AR$810",
+        imagen: "/image/paleta-maquillaje.jpeg"
+    },
+    {
+        nombre: "Delineador Líquido",
+        precio: "AR$940",
+        imagen: "/image/rimel.jpeg"
+    },
+    {
+        nombre: "Rímel Waterproof",
+        precio: "AR$780",
+        imagen: "/image/rimel2.jpeg"
+    },
+    {
+        nombre: "Base Líquida",
+        precio: "AR$810",
+        imagen: "/image/baseliquila.webp"
+    },
+    {
+        nombre: "Esmalte Smokey Blue",
+        precio: "AR$810",
+        imagen: "/image/base-dior.jpeg"
+    },
+    {
+        nombre: "Labial Ultracor",
+        precio: "AR$810",
+        imagen: "/image/labial-ultracor.png"
+    },
+    {
+        nombre: "Brillo Pink",
+        precio: "AR$810",
+        imagen: "/image/brillo-pink.webp"
+    },
+    {
+        nombre: "Kit Sombras de Ojos",
+        precio: "AR$810",
+        imagen: "/image/sombras.webp"
+    },
+    
+]
+
+module.exports =listaDeProductos2;
